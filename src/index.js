ada22daae627d0507e30f935998c03604f4f6fb2
voronovanasta <110159859+voronovanasta@users.noreply.github.com>
2023-02-17 08:54:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
-}
+
+    let numberArr = Math.abs(n).toString().split('');
+    let newNumberArr =[]
+    
+    
+    for(let i=numberArr.length-1; i>=0 ; i--){
+      
+        newNumberArr.push(numberArr[i]);
+        
+    }
+    
+    return +newNumberArr.join('')
+    }
